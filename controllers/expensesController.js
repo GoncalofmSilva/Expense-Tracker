903f7ec4629e8682b1ee95b8142f71f35eb6433d
Goncalo Silva <goncalofmsilva2004@gmail.com>
2025-11-05 00:14:04 +0000

    comment

--- controllers/expensesController.js
+++ controllers/expensesController.js
@@ -126,4 +126,7 @@ export const summaryOfExpensesForMonth = async (req, res) => {
   }
 }
 
-// TODO: Summary of Expenses for specific month (current year)
+// Addional features
+// Add expensive categories and allow users to filter expenses by category
+// Allow users to set a budget for each month and show a warning when the user exceeds the budget.
+// Allow users to export expenses to a CSV file.
